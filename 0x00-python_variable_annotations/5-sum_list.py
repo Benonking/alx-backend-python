92c4ab06f8171194466274a106f918518e931b19
Benonking <benonkighoma@gmail.com>
2023-08-04 19:42:55 +0300

    pyhton type annotation
    sum elements of a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' function takes ina list of flaots and adds them '''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''add memenbers of a list(floats)'''
+    return sum(input_list)
