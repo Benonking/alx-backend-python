0c04c785b4db3596de7fc28ac3d16dc80bec588c
Benonking <benonkighoma@gmail.com>
2023-08-04 19:55:37 +0300

    pyhton type annotation
    sum list elements of diffrent types

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''function takes a list of loats and ints and returns the sum'''
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, loat]]) -> float:
+    '''retun sum'''
+    return sum(mxd_lst)
