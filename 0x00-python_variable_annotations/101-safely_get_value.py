5ec95024f6725733795e25a0c32a5702b7958f5c
Benonking <benonkighoma@gmail.com>
2023-08-05 20:28:16 +0300

    Python type annotations
    get value from dictionary

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+''' safely get value from dictionary'''
+from typing import Dict, Mapping, Any, Union, Optional, TypeVar
+
+T = TypeVar("T")
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Optional[T] = None) -> [Union, T]:
+    '''
+    Safely gt value from dictionary with TypeVar
+    Args:
+      dict: Dictionary to get value from
+      key: Key to get value from
+      default: default value if not found
+    Returns:
+      value
+    '''
+    if key in dict:
+        return dct[key]
+    else:
+        return default
