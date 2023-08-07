d7c15f893bf6314a1db2a2b9bb5812edc473796a
Benonking <benonkighoma@gmail.com>
2023-08-07 22:31:51 +0300

    run concurrent commit

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''
+
+Run Multiple cocurents all at once
+'''
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay) -> List:
+    '''
+    Return list of delays in ascending order(order of complition)
+    Args:
+        n: number of times of calling the imported funtion
+        max_delay: arg of imported function wait_random
+    '''
+    res, delays = [], []
+    for _ in range(n):
+        delays.append(wait_random(max_delay))
+    # get awaitables in order of completion
+    for delay in asyncio.as_completed(delays):
+        result = await delay
+        res.append(result)
+    return res
