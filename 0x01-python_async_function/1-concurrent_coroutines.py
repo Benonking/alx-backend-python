a5d8d33c2c4d92ea67ed4f16fa3757feb313c4fa
Benonking <benonkighoma@gmail.com>
2023-08-07 23:30:44 +0300

    run concurrent commit

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,7 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay) -> List:
+async def wait_n(n: int, max_delay: int) -> List:
     '''
     Return list of delays in ascending order(order of complition)
     Args:
