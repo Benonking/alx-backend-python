132e87803948a291abd99238076e48a9b6963183
Benonking <benonkighoma@gmail.com>
2023-08-08 20:19:56 +0300

    measure run time of an async funtion

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 '''
-Calculate Run time of four 
+Calculate Run time of four
 '''
+import asyncio
+from time import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''
+    Measure run time of a funtion
+    '''
+    start = time()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    end = time()
+    return end - start
