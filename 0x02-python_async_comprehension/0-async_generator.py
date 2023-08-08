aa3f9b2852b7b79aafb36dcb3b04afdea67f798c
Benonking <benonkighoma@gmail.com>
2023-08-08 19:16:11 +0300

    loop coroutine ten times

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''
+Coroutine loops ten times and yeads random number
+'''
+import random
+import asyncio
+
+
+async def async_generator():
+    '''
+    Loop couritne ten times and sleep 
+    for one second after every loop
+    '''
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield genRandom()
+
+
+def genRandom() -> int:
+    rand = random.uniform(0, 10)
+    return rand
