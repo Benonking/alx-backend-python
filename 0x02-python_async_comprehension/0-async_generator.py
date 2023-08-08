3d8db050c1b2b180c1372013282298d9483a6740
Benonking <benonkighoma@gmail.com>
2023-08-08 19:28:07 +0300

    loop coroutine ten times

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -12,7 +12,7 @@ async def async_generator() -> Generator[float, None, None]:
     Loop couritne ten times and sleep
     for one second after every loop
     '''
-    for _ in range(10):
+    for _ in range(0, 10):
         await asyncio.sleep(1)
         yield genRandom()
 
