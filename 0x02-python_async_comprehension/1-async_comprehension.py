1aaf55645f8938c1672f4ab0ab4ea9fcfa2c665f
Benonking <benonkighoma@gmail.com>
2023-08-08 19:53:18 +0300

    gemrate async-comprehension from Genrator object

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Generate async comprehension from generator object
+'''
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''get Genrotor object and return it'''
+    return [i async for i in async_generator()]
