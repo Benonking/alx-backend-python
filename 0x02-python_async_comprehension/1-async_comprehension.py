af5a7c68942443c37a952fb2830aa64e8073fb52
Benonking <benonkighoma@gmail.com>
2023-10-11 07:16:40 +0300

    correct function anotation from List[int] to List[float]

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
-    '''get Generator object and return it'''
+async def async_comprehension() -> List[float]:
+    '''get Generator object and return it inside a list comprehension'''
     return [i async for i in async_generator()]
