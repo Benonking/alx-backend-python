edd246bc02a3c235fb9693f22ece6ef304231c69
Benonking <benonkighoma@gmail.com>
2023-10-30 09:02:05 +0300

    unit test to check result of access_nested map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,9 +16,9 @@ class TestAccessNestedmap(unittest.TestCase):
     '''
     @parameterized.expand(
         [
-            ({"a": 1}, ["a"], 1),
-            ({"a": {"b": 2}}, ["a"], {"b": 2}),
-            ({"a": {"b": 2}}, ["a", "b"], 2),
+            ({"a": 1}, ("a"), 1),
+            ({"a": {"b": 2}}, ("a"), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
         ]
     )
     def test_access_nested_map(
